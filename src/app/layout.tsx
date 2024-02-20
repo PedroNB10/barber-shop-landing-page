8dbaf150f07587393f2df12a8a3511b1d508ed96
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-02-20 12:08:56 -0300

    Commit inicial.

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Barba Rolling",
+  description: "Landing page destinada a divulagação de produtos e serviços da Barbearia Barba Rolling",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" >
+      <head>
+        <link
+          rel="icon"
+          href="/icon/favicon.ico"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
+
+      <body className={`${inter.className}`} >
+        <Header />
+        {children}
+        <Footer />
+      </body>
+
+    </html>
+  );
+}
