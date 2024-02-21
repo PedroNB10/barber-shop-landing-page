27d9194062167e62de77f64d5482a627233119e5
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-02-21 20:57:51 -0300

    adicionei o texto alternativo das imagens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import { Carousel, Slide } from "./components/Carousel";
 export default function Home() {
 
   let slides: Slide[] = [
-    { src: "img/carrossel-01.jpg", alt: "Placeholder 1" },
-    { src: "img/carrossel-02.jpg", alt: "Placeholder 2" },
-    { src: "img/carrossel-03.jpg", alt: "Placeholder 3" },
+    { src: "img/carrossel-01.jpg", alt: "Rapaz cortando o cabelo de um outro homem." },
+    { src: "img/carrossel-02.jpg", alt: "Corte em foco de um outro cabeleireiro." },
+    { src: "img/carrossel-03.jpg", alt: "Barbeiro realizando um corte preciso na barba do cliente." },
   ];
 
   return (
