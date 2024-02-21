7ffbeea50d0b97a69e0521f6a7a06595fd0ea4a5
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-02-21 20:36:40 -0300

    adicionei o carrossel responsivo alinhado ao header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 
+import { Carousel, Slide } from "./components/Carousel";
+
 
 export default function Home() {
+
+  let slides: Slide[] = [
+    { src: "img/carrossel-01.jpg", alt: "Placeholder 1" },
+    { src: "img/carrossel-02.jpg", alt: "Placeholder 2" },
+    { src: "img/carrossel-03.jpg", alt: "Placeholder 3" },
+  ];
+
   return (
 
     <>
+
+
+      <Carousel slides={slides} />
+
+
+
 
     </>
 
